035855aceae832c014afe91aa995e090348b4f0e
ANIL KUMAR SINGH KUSHWAHA <90541938+anil9450@users.noreply.github.com>
2024-03-21 00:33:50 +0530

    Half Part Completed

--- first_Project/App.tsx
+++ first_Project/App.tsx
@@ -1,24 +1,47 @@
-import { useState } from 'react';
-import {Image, StatusBar, TextInput, TouchableOpacity} from 'react-native';
+import {useState} from 'react';
+import {
+  Image,
+  ScrollView,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 import {KeyboardAvoidingView, StyleSheet, Text, View} from 'react-native';
 import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
 
 export default function App() {
-
-  const [task,setTask] = useState();
-  const [taskItems,setTaskItems] = useState([]);
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
 
   const handleAddTask = () => {
-Keyboard.dismiss();
-if(task){
-  setTaskItems([...taskItems,task])
-  setTask(null);
-}
-  }
+    Keyboard.dismiss();
+    if (task) {
+      setTaskItems([...taskItems, task]);
+      setTask(null);
+    }
+  };
 
   return (
     <View style={styles.container}>
+      <ScrollView style={{flexGrow: 1}}>
+        <Text
+          style={{
+            backgroundColor: '#0AD6A0',
+            marginTop: 25,
+            textAlign: 'center',
+            fontSize: 20,
+            padding: 10,
+            fontWeight: '700',
+          }}>
+          My ToDo App
+        </Text>
+
+        <TouchableOpacity style={styles.removealltext}>
+          <Text style={styles.removealltext}>Remove All</Text>
+        </TouchableOpacity>
+      </ScrollView>
+
       <KeyboardAvoidingView style={StyleSheet.writeTaskWrapper}>
         <TextInput
           placeholder="Enter your Task"
@@ -27,14 +50,24 @@ if(task){
           onChangeText={text => {
             setTask(text);
           }}
-        /> 
+        />
 
-        <TouchableOpacity onPress={()=>handleAddTask()}>
-          <Image source={require("./images/plus.png")}/>
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <Image source={require('./images/plus.png')} />
         </TouchableOpacity>
       </KeyboardAvoidingView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    /* alignItems: 'center', */
+  },
+  removealltext:{
+    fontSize: 17,
+
+  }
+});
